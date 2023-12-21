3e0fdacb3b761cda1ccf5f9cbad9dab15a14119d
terklnwza007 <105853864+terklnwza007@users.noreply.github.com>
2023-12-21 09:19:43 +0700

    Update skill_robot.cpp config

--- skill_robot.cpp
+++ skill_robot.cpp
@@ -7,10 +7,12 @@ int main(){
     cin >> way;
     int error;
     cin >> error;
+    
     int count_N=0;
     int count_W=0;
     int count_S=0;
     int count_E=0;
+    
     for(int i =0; i< way.length(); i++){
         if(way[i]=='N'){
             count_N++;
@@ -27,42 +29,39 @@ int main(){
         }
     }
     
-
     for(int i=0;i<error; ++i){
-        if((count_N > count_S )&& count_S !=0){
+        if((count_N > count_S )&& count_S !=0)
             count_S --;
-            
-        }else if ((count_S > count_N) && count_N !=0){
+        else if ((count_S > count_N) && count_N !=0)
             count_N--;
-        }
-        else if ((count_E > count_W) && count_W !=0){
+        else if ((count_E > count_W) && count_W !=0)
             count_W--;
-        }else if((count_W > count_E) && count_E !=0){
+        else if((count_W > count_E) && count_E !=0)
             count_E--;
-        }else if((count_N == count_S) && (count_S !=0 && count_S!=0)){
+        else if((count_N == count_S) && (count_S !=0 && count_S!=0))
             count_N--;
-        }else if((count_E == count_W) && (count_E !=0 && count_W!=0)){
+        else if((count_E == count_W) && (count_E !=0 && count_W!=0))
             count_E--;
-        }else if(count_N!=0) count_N--;
-        else if(count_S!=0) count_S--;
-        else if(count_E!=0) count_E--;
-        else if(count_W!=0) count_W--;
+        else if(count_N!=0) 
+            count_N--;
+        else if(count_S!=0) 
+            count_S--;
+        else if(count_E!=0) 
+            count_E--;
+        else if(count_W!=0) 
+            count_W--;
         
-
-
     }
 
-    for(int i =0;i< count_E; i++){
-            x++;
-        }
-        for(int i =0;i< count_W; i++){
-            x--;
-        }for(int i =0;i< count_S; i++){
-            y--;
-        }for(int i =0;i< count_N; i++){
-            y++;
-        }
-
-
+    for(int i =0;i< count_E; i++)
+        x++;
+    for(int i =0;i< count_W; i++)
+        x--;
+    for(int i =0;i< count_S; i++)
+        y--;
+    for(int i =0;i< count_N; i++)
+        y++;
+        
     cout << (abs(x)+abs(y))*2;
+    return 0;
 }
